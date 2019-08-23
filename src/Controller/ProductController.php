8a801e7a4ae54c324aa3d42e6e937de4e4298095
pierrehervo <pierre.hervo@laposte.net>
2019-08-23 17:30:58 +0200

    tp liste produits dans product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -32,4 +32,67 @@ class ProductController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/product/demo")
+     */
+    public function demo()
+    {
+        //Récuperer le repository de l'entité Product
+        $productRepository = $this->getDoctrine()->getRepository(Product::class);
+
+        //Récupérer tous les produits
+        $products = $productRepository->findAll();
+
+        //Réccupérer le produit 2
+        $product2 = $productRepository->find(2);
+
+        //Récuperer le produit qui se nomme iphone X
+        $product3 = $productRepository->findOneByName('iPhone X');
+
+        //Récupérer tous les produits qui coutent 1500euros exactement
+        $products1500 = $productRepository->findByPrice(1500); 
+
+        return $this->render('product/demo.html.twig', [
+            'products'=>$products,
+            'product2'=>$product2,
+            'product3'=>$product3,
+            'products1500'=>$products1500,
+        ]);
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function show(Product $product) //Dans la version commentée il faudrait mettre $id à la place de Product $product
+    {
+        /*$product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+
+        //Si le produit n'existepas, on renvoie une 404
+        if(!$product){
+            throw $this->createNotFoundException();
+        } */
+
+        return $this->render('product/show.html.twig', [
+            'product' =>$product,
+        ]);
+    }
+
+    /**
+     * @Route("/product")
+     */
+    public function list()
+    {
+        //Récuperer le repository de l'entité Product
+        $productRepository = $this->getDoctrine()->getRepository(Product::class);
+
+        //Récupérer tous les produits
+        $products = $productRepository->findAll();
+
+        return $this->render('product/list.html.twig', [
+            'products' =>$products,
+        ]);
+    }
 }
